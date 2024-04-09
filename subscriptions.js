3c327b1bc53b2294b182ac89505023b6bc6eab59
Martin Cowie <martin.cowie@diffusiondata.com>
2024-04-09 11:57:57 +0100

    Save subscriptions with the document

--- subscriptions.js
+++ subscriptions.js
@@ -26,6 +26,40 @@ export class Subscriptions {
             row.remove();
             this.unsubLambda(topicPath, cell);
         }
+
+        
+        Excel.run(async (context) => {
+            const KEY = "subscriptions";
+            const newEntry = {
+                tm: new Date().getTime(),
+                topicPath: topicPath, 
+                cell: cell
+            };
+
+            const settings = context.workbook.settings;
+
+            const setting = settings.getItemOrNullObject(KEY); // _sigh_
+            await context.sync();
+    
+            if (setting.isNullObject) {
+                console.log(`${KEY} is absent`);
+
+                settings.add(KEY, [newEntry]);
+                await context.sync();
+            } else {
+                console.log(`${KEY} is present`);
+                setting.load("value");
+                await context.sync();
+
+                setting.value.push(newEntry);
+                settings.add(KEY, setting.value);
+                await context.sync();
+
+                console.log(`setting: ${JSON.stringify(setting.value)}`);
+            }
+        });
+
+
     }
 
     
